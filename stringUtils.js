d6d2e59efe8a8f8ab59b8bbbd6defb2c609ffdfc
Futursaith <saith_7@hotmail.fr>
2021-03-04 14:38:52 +0100

    exo5

--- stringUtils.js
+++ stringUtils.js
@@ -1,4 +1,9 @@
 const reverseStr = (msg) => {
   return msg.split('').reverse().join('')
 }
+const isPalindrome = (msg) => {
+  return msg === reverseStr(msg);
+}
 console.log(reverseStr('Hello!'))
+console.log(isPalindrome('Hello!'));
+
