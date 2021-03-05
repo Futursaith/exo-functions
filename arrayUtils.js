a9c46cf9618b6267142b423d33235a7fd53cdff1
Futursaith <saith_7@hotmail.fr>
2021-03-05 12:24:32 +0100

    exo6

--- arrayUtils.js
+++ arrayUtils.js
@@ -1,5 +1,5 @@
 const biggest = (tab) => {
-  let bignb = 0
+  let bignb = tab[0]
   for (i = 0; i < tab.length; i++) {
     if (bignb < tab[i]) {
       bignb = tab[i]
@@ -9,3 +9,18 @@ const biggest = (tab) => {
 }
 
 console.log(biggest([99, 100, 101, 1]))
+
+
+
+const sortAscend = (tab) => {
+  return tab.sort((a, b) => a - b)
+}
+
+console.log(sortAscend([99, 110, 101, 1]))
+
+
+
+const makeUnique = (tab) => {
+  return [...new Set(tab)]
+}
+console.log(makeUnique([1, 2, 1, 3, 2, 4, 5, 7, 5, 1]))
