7992fa66399f3606f5779d81a34f76d39ce12955
Futursaith <saith_7@hotmail.fr>
2021-03-07 20:29:27 +0100

    pyramid fonction

--- pyramid.js
+++ pyramid.js
@@ -1,13 +1,23 @@
+/**
+ * 
+ * définition de la fonction : on détermine ce que fait la fonction
+ * 
+ * @déscription : affiche une pyramide dans la console (terminal)
+ * 
+ * @param {number} nbBase nombre d'éléments VARIABLE qui sera à la base de la pyramide
+ * @param {boulean} reverse valeur vrai/faux détermine si la pyramide est à l'endroit ou  à l'envers
+ */
 const showStars = (nbBase, reverse) => {
+  // définit un string vide
   let str = ''
-  if (!reverse) {
+  if (!reverse) { // si on est à l'inverse de reverse, c'est à dire pyramide à l'endroit
     for (let i = 1; i <= nbBase; ++i) {
       str += '#'.repeat(i)
-      if (i !== nbBase) {
+      if (i !== nbBase) { // tant que le tour de boucle n'est pas égal à nbBase alors on saute une ligne
         str += '\n'
       }
     }
-  } else {
+  } else { // pyramide à l'envers
     for (let i = nbBase; i >= 1; --i) {
       str += '#'.repeat(i)
       if (i !== 1) {
@@ -17,7 +27,12 @@ const showStars = (nbBase, reverse) => {
   }
   return str
 }
-const nbBase = 20
 
-let str2 = showStars(nbBase, false)
-console.log(str2)
+// appel de la fonction : on utilise la fonction
+const nbBase = 20
+/**
+ * la variable resultat va accueillir
+ * le resultat de l'utilisation de la fonction showStars
+ */
+let resultat = showStars(nbBase, true)
+console.log(resultat)
